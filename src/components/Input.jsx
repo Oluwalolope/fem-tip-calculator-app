199e86c3a36689785c14f96948079c81cbefc6ca
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2025-06-24 19:39:56 +0100

    Made it mobile responsive

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ const Input = ({label}) => {
     return (
         <div className="inputWrapper">
             {label && <label className="label" htmlFor={label}>{label}</label>}
-            <input type="number" id={label} placeholder="0" className={label == "Bill" ? 'bill' : 'numberOfPeople'}/>          
+            <input type="number" id={label} placeholder="0" className={label == "Bill" ? 'bill' : 'numberOfPeople'} step={label == "Bill" ? '0.01' : '0'} min="0" />          
         </div>
     )
 }
